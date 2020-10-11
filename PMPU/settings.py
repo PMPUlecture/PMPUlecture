1b8afc69a13a32ca4ba75c0f390cb0fd21fb0a0f
Kiruha01 <liss-2001g@mail.ru>
2020-10-11 19:26:11 +0300

    dfghjkw

--- PMPU/settings.py
+++ PMPU/settings.py
@@ -120,4 +120,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 django_heroku.settings(locals())
