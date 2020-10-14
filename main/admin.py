f8dd8e093ea675ccaf24ab7ed64300bfc450423c
Kiruha01 <liss-2001g@mail.ru>
2020-10-14 23:12:01 +0300

    Красивая админка

--- main/admin.py
+++ main/admin.py
@@ -3,8 +3,37 @@ from .models import Subject, Lecturer, Programme, Materials
 
 # Register your models here.
 
-admin.site.register(Subject)
-admin.site.register(Lecturer)
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'term', 'programme')
+    list_filter = ('term', 'programme')
+    fields = ['name', ('term', 'programme')]
+
+
+class MaterialsInline(admin.TabularInline):
+    model = Materials
+
+
+@admin.register(Lecturer)
+class LecturerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'display_subjects')
+
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'subject',)
+        }),
+        ('URLs', {
+            'fields': ('photo_url', 'apmath_url', 'vk_discuss_url')
+        }),
+    )
+    inlines = [MaterialsInline]
+
+
 admin.site.register(Programme)
-admin.site.register(Materials)
+
+@admin.register(Materials)
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'subject', 'lecturer')
+    list_filter = ('type', 'subject', 'lecturer')
+
 
--- main/models.py
+++ main/models.py
@@ -7,11 +7,27 @@ class Programme(models.Model):
     name = models.CharField(max_length=100)
     img_url = models.URLField(max_length=256)
 
+    def __str__(self):
+        return self.name
+
+    def as_dict(self):
+        return self.name
+
 
 class Subject(models.Model):
     name = models.CharField(max_length=100)
     programme = models.ForeignKey(Programme, on_delete=models.SET_NULL, null=True)
     term = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.name + ' (' + str(self.term) + ')'
+
+    def as_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'term': self.term,
+        }
 
 
 class Lecturer(models.Model):
@@ -21,12 +37,27 @@ class Lecturer(models.Model):
     vk_discuss_url = models.URLField(max_length=256, null=True)
     photo_url = models.URLField(max_length=256, null=True)
 
-    def as_dict(self):
-        return {
+    def as_dict(self, *args, apmath=False, photo=False, vk_discuss=False):
+        dicte = {
             'id': self.id,
             'name': self.name,
-            'subject': [{'name': item.id, 'subject_id': item.name} for item in self.subject.all()]
+            'subjects': [item.as_dict() for item in self.subject.all()]
         }
+        if apmath:
+            dicte['apmath'] = self.apmath_url
+        if photo:
+            dicte['photo'] = self.photo_url
+        if vk_discuss:
+            dicte['vk_discuss_url'] = self.vk_discuss_url
+        return dicte
+
+    def __str__(self):
+        return self.name
+
+    def display_subjects(self):
+        return ', '.join([subjects.__str__() for subjects in self.subject.all()[:3]])
+
+    display_subjects.short_description = 'Subjects'
 
 
 class Materials(models.Model):
@@ -34,8 +65,21 @@ class Materials(models.Model):
         ABSTRACT = ("abstract", "конспект")
         QUESTIONS = ("questions", "вопросы")
         TEST = ("test", "контрльная")
-    type = models.CharField(max_length=16, choices=TypeOfMaterial.choices)
-    lecturer = models.ForeignKey(Lecturer, on_delete=models.SET_NULL, null=True)
-    subject = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True)
-    link = models.URLField(max_length=256, null=True)
     name = models.CharField(max_length=256, null=True)
+    type = models.CharField(max_length=16, choices=TypeOfMaterial.choices)
+    subject = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True)
+    lecturer = models.ForeignKey(Lecturer, on_delete=models.SET_NULL, null=True)
+    link = models.URLField(max_length=256, null=True)
+
+
+    def __str__(self):
+        return self.name
+
+    def as_dict(self):
+        return {
+            'name': self.name,
+            'type': self.type,
+            'subject': self.subject,
+            'lecturer': self.lecturer,
+            'url': self.link
+        }
