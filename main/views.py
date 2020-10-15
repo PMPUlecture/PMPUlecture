683a742dd522704dcac54e39b0c40e1aa6fdaa4c
Kiruha01 <liss-2001g@mail.ru>
2020-10-16 00:13:50 +0300

    Fuck

--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.core import serializers
-from .models import Lecturer, Subject
+from .models import Lecturer, Subject, Programme, Materials
 
 # Create your views here.
 
@@ -15,4 +15,13 @@ def get_lecturer(req):
     print(lecturers)
     #response_data = serializers.serialize('json', lecturers)
     return JsonResponse({'lecturers': [obj.as_dict() for obj in lecturers]}, safe=False)
+
+
+def get_programmes(req):
+    progr = Programme.objects.filter(degree='bachelor')
+    print(progr)
+    resp = JsonResponse({'programmes': [obj.as_dict() for obj in progr]})
+    resp.setdefault('Access-Control-Allow-Origin', '*')
+    #resp.setdefault('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers')
+    return resp
 
