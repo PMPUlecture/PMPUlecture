c40083f4bc66af6b3e9784c40eeaa2f988df9c9a
Kiruha01 <liss-2001g@mail.ru>
2020-10-24 00:50:32 +0300

    Add /struct api
    /struct?programme=### - получает структуру направления по семестрам

--- main/api/urls.py
+++ main/api/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^programmes/$', views.ProgListView.as_view()),
-    url(r'^subjects/$', views.SubjectView.as_view()),
+#     url(r'^subjects/$', views.SubjectView.as_view()),
     path('lecturer/', views.DetailLecturer.as_view()),
+    path('struct/', views.ListUsers.as_view()),
     ]
--- main/api/views.py
+++ main/api/views.py
@@ -1,8 +1,9 @@
 from rest_framework import generics, views
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from ..models import Programme, Subject, Lecturer
-from .serializers import ProgrammeSerializer, SubjectSerializer, LecturerSerializer
+from .serializers import ProgrammeSerializer, SubjectSerializer, LecturerSerializer, LecturerStructSerializer
 
 
 class ProgListView(generics.ListAPIView):
@@ -19,9 +20,25 @@ class DetailLecturer(generics.RetrieveAPIView):
     # теперь выбирает объект по url параметру /?pk=1
     def retrieve(self, request, *args, **kwargs):
         idLecturer = request.query_params['pk']
-        return Response(self.serializer_class(Lecturer.objects.get(pk=idLecturer)).data)
+        return Response(self.serializer_class(self.queryset.get(pk=idLecturer)).data)
 
 
-class SubjectView(generics.ListAPIView):
+# @api_view()
+# def hello_world(request):
+#     return Response({"message": "Hello, world!"})
+
+class ListUsers(views.APIView):
     queryset = Subject.objects.all()
-    serializer_class = SubjectSerializer
+    lecSer = LecturerStructSerializer
+    subSer = SubjectSerializer
+
+    def get(self, request):
+        if request.query_params.get('programme'):
+            programme = generics.get_object_or_404(Programme.objects.all(), name=request.query_params['programme'])
+            res = Response([{'term': term, 'subjects':
+                [self.subSer(subject).data for subject in self.queryset.filter(term=term, programme=programme)]} for
+                            term in range(1, 9)])
+        else:
+            res = Response({'error': "No Such Programme"})
+        res.setdefault('Access-Control-Allow-Origin', '*')
+        return res
