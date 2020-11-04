17ffc87dcb86194277e8f943c34ee24d5d31c33d
kolbacer <mitjas01@yandex.ru>
2020-11-04 22:13:08 +0300

    setup frontend

---
+++ frontend/PMPU/src/views/Home.vue
@@ -0,0 +1,55 @@
+<template>
+ <!-- id="app" не нужен -->
+    <!-- Image and text -->
+    <div class="container-fluid">
+      <div class="row vh-100">
+        <div class="col-3">
+
+          <div class="card h-100">
+            <div class="card-body d-flex flex-column justify-content-between">
+
+              <div class="list-group list-group-flush">
+                <a class="list-group-item list-group-item-action" href="https://vk.com/pmpu_news">ПМ-ПУ СМИ</a>
+                <a class="list-group-item list-group-item-action" href="https://vk.com/sspmpu">Студсовет ПМ-ПУ</a>
+                <a class="list-group-item list-group-item-action" href="#">Студсовет2 ПМ-ПУ</a>
+              </div>
+
+              <div class="list-group list-group-flush">
+                <a class="list-group-item list-group-item-action" href="#">1</a>
+                <a class="list-group-item list-group-item-action" href="#">2</a>
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+        <div class="col">
+
+          <div class="card h-100">
+            <div class="card-body">
+              <h5 class="card-title">Название карточки</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data() {
+  },
+  methods: {
+
+  }
+}
+
+</script>
