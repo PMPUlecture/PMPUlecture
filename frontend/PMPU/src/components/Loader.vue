f7587d70d67f86a9e8cb20428a70696deb81e485
Kiruha01 <liss-2001g@mail.ru>
2020-11-27 23:48:41 +0300

    Build

---
+++ frontend/PMPU/src/components/Loader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="spinner-border text-primary loader" role="status">
+    <span class="sr-only">Loading...</span>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Loader"
+}
+</script>
+
+<style scoped>
+.loader{
+
+}
+</style>
