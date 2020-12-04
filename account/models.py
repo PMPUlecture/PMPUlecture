83367d618784ba0c73b50fae2841fea375f5cf88
Kiruha01 <liss-2001g@mail.ru>
2020-12-04 21:22:49 +0300

    Fix admin

--- account/models.py
+++ account/models.py
@@ -31,4 +31,4 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     def __str__(self):
-        return self.email
+        return self.first_name + ' ' + self.last_name
