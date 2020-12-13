c01ad297b5595dce7a4e8a680139b7b7153c4fe2
GrigorevGrigorii <nigddd@gmail.com>
2020-12-13 11:04:41 +0300

    Edit redirect to https

--- main/views.py
+++ main/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, redirect
+from django.conf import settings
 
 
 def index(request):
-    if request.META.get('wsgi.url_scheme') == 'http':
-        return redirect('https://pmpulecture.herokuapp.com' + request.path)
+    if request.META.get('wsgi.url_scheme') == 'http' and not settings.DEBUG:
+        return redirect('https://' + request.META.get('HTTP_HOST') + request.path)
 
     return render(request, 'index.html', context={'user': request.user})
-
-
