3983e310059251b5734aea05c96114e228effe8d
GrigorevGrigorii <nigddd@gmail.com>
2021-01-30 16:38:27 +0300

    Add login attempt notification

--- PMPU/pipeline.py
+++ PMPU/pipeline.py
@@ -1,6 +1,36 @@
 from django.shortcuts import redirect
 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+import os
+
 
 def auth_allowed(backend, details, response, **kwargs):
     if not backend.auth_allowed(response, details):
+        msg = MIMEMultipart()
+        message = f"""Кто-то пытался зайти не под студенческой почтой! Вот его данные:
+        
+        username: {details['username']}
+        email: {details['email']}
+        fullname: {details['fullname']}
+        first_name: {details['first_name']}
+        last_name: {details['last_name']}
+        """
+
+        password = os.environ.get('email_password')
+        msg['From'] = "pmpulecture@bk.ru"
+        msg['To'] = "pmpulecture@bk.ru"
+        msg['Subject'] = "Кто-то пытался зайти не под студенческой почтой!"
+
+        msg.attach(MIMEText(message, 'plain'))
+
+        server = smtplib.SMTP('smtp.bk.ru: 587')
+        server.starttls()
+        server.login(msg['From'], password)
+
+        server.sendmail(msg['From'], msg['To'], msg.as_string())
+
+        server.quit()
+
         return redirect('/login/?error=2')
