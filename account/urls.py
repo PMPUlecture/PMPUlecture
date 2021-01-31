ede2c1ca11f73139dbe3b62b5523b69ad699941e
GrigorevGrigorii <nigddd@gmail.com>
2021-01-31 07:58:18 +0300

    Change redirect url for logout

--- account/urls.py
+++ account/urls.py
@@ -2,10 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.RedirectIfLogin.as_view()),
-    path('login', views.LogInView.as_view()),
-    path('login/', views.LogInView.as_view()),
-    path('logout', views.LogOutView.as_view()),
     path('logout/', views.LogOutView.as_view()),
     path('', include('social_django.urls', namespace='social')),
 ]
--- account/views.py
+++ account/views.py
@@ -1,35 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 
-from django.http import Http404, JsonResponse
 from django.views import View
-from django.contrib.auth import authenticate, login, logout
-
-from .models import User
-
-
-class RedirectIfLogin(View):
-    def get(self, request, *args, **kwargs):
-        return redirect('/')
-
-
-class LogInView(View):
-    def get(self, request, *args, **kwargs):
-        return 'no'
-
-    def post(self, request, *args, **kwargs):
-        email = request.POST['email']
-        password = request.POST['password']
-        user = authenticate(request, email=email, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('/')
-        else:
-            return redirect('/login/?error=1')
+from django.contrib.auth import logout
 
 
 class LogOutView(View):
     def get(self, request, *args, **kwargs):
-        print("herer")
         if request.user.is_authenticated:
             logout(request)
-        return redirect('/')
+        return redirect(request.META.get('HTTP_REFERER') if 'HTTP_REFERER' in request.META else '/')
