97d408c9c0006d613bb9d237189e0fd320e87e0d
Kiruha01 <liss-2001g@mail.ru>
2021-02-09 17:32:57 +0300

    change crown to eye

--- frontend/PMPU/src/components/lecturer/materials/MaterialLink.vue
+++ frontend/PMPU/src/components/lecturer/materials/MaterialLink.vue
@@ -2,7 +2,7 @@
   <div>
     <div class=" d-flex">
       <a target="_blank" :href="source.link" class="d-inline-flex justify-content-between flex-grow-1">
-        <p class="m-0 mr-auto text-wrap">{{source.name}} <i v-if="source.only_authorized_users" class="fas fa-crown ml-1 text-warning" title="Этот материал виден только студентам"></i></p>
+        <p class="m-0 mr-auto text-wrap">{{source.name}} <i v-if="source.only_authorized_users" class="fas fa-eye ml-1 text-warning" title="Этот материал виден только студентам"></i></p>
         <p class="m-0 ml-3 text-muted" >{{source.year_of_relevance}}</p>
       </a>
       <div v-if="source.is_author" class="m-0 ml-2">
