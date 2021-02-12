a525c1404be40dc371574163013c7aa29d1c4ab4
Kiruha01 <liss-2001g@mail.ru>
2021-02-12 14:54:01 +0300

    Fix localhost

--- frontend/PMPU/src/views/variables.js
+++ frontend/PMPU/src/views/variables.js
@@ -1,10 +1,3 @@
 export default {
-  url: 'http://127.0.0.1:8000',
-  user: {
-    "is_authenticated": false,
-    "email": "",
-    "first_name": "",
-    "last_name": "",
-    "is_admin": false
-  },
+  url: '',
 }
