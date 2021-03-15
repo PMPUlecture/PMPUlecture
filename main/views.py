08a0c62740d8feffe27102cc279abececdc9acdc
Лисов Кирилл <liss-2001g@mail.ru>
2021-03-15 11:15:20 +0300

    Downgrade https

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 
 def index(request):
-    if request.META.get('wsgi.url_scheme') == 'http' and not settings.DEBUG:
-        return redirect('https://' + request.META.get('HTTP_HOST') + request.path)
+    if request.META.get('wsgi.url_scheme') == 'https' and not settings.DEBUG:
+        return redirect('http://' + request.META.get('HTTP_HOST') + request.path)
 
     return render(request, 'index.html', context={'user': request.user})
