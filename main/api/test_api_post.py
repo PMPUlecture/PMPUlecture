494b2dc8f40585a7e9967810fc168775760e7521
Kiruha01 <liss-2001g@mail.ru>
2021-04-24 16:16:35 +0300

    Add tests for POST subjects

---
+++ main/api/test_api_post.py
@@ -0,0 +1,37 @@
+from django.test import TestCase
+from django.test.client import Client
+import json
+
+from main.models import Programme, Subject, Lecturer, Materials
+from account.models import User
+
+
+class SubjectTestCase(TestCase):
+    def setUp(cls):
+        Programme.objects.create(name="programme", degree="bachelor")
+        User.objects.create_superuser("r@r.com", "password")
+
+    def test_create_subject(self):
+        c = Client()
+        r = c.post('/api/subjects/', data=json.dumps({'programme': 1, 'term': 1, 'name': 'subject1'}),
+                   content_type="application/json").json()
+        self.assertDictEqual(r, {'status': 'error', 'error': 'Permission denied'}, msg='not auth')
+
+        c.login(email="r@r.com", password="password")
+
+        r = c.post('/api/subjects/', data=json.dumps({'programme': 1, 'term': 1, 'name': 'subject1'}),
+                   content_type="application/json").json()
+        self.assertDictEqual(r, {'status': 'ok'}, msg='auth')
+
+    def test_trying_create_incorrect_subject(self):
+        c = Client()
+        c.login(email="r@r.com", password="password")
+        r = c.post('/api/subjects/', data=json.dumps({'programme': 5, 'term': 1, 'name': 'subject1'}),
+                   content_type="application/json").json()
+
+        self.assertDictEqual(r, {'status': 'error', 'error': 'there is no such programme'}, msg="incorrect programme")
+
+        r = c.post('/api/subjects/', data=json.dumps({'programme': 1, 'term': 10, 'name': 'subject1'}),
+                   content_type="application/json").json()
+        self.assertDictEqual(r, {'status': 'error', 'error': 'term should be between 1 and 8'}, msg="incorrect term")
+
--- main/tests.py
+++ main/tests.py
@@ -1,5 +1,5 @@
 from .test_models import ModelsTestCase
-from .api.test_api import LecturerApiTestCase, SubjectsApiTestCase, ProgrammesApiTestCase
+from .api.test_api_get import LecturerApiTestCase, SubjectsApiTestCase, ProgrammesApiTestCase
 
 __test__ = {"Models": ModelsTestCase, "ApiLecturer": LecturerApiTestCase, "ApiSubjects": SubjectsApiTestCase,
             "ApiProgrammes": ProgrammesApiTestCase}
